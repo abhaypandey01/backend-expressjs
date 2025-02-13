a4bfdbf9a836852d728ba5c9fa60ba9ab2dc065f
Abhay Pandey <abhaymp09@gmail.com>
2025-02-13 22:11:17 +0530

    yes commiting

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 4000
+// require('dotenv').config()
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/login', (req,res)=>{
-    res.send('<h1>Yo no log in here ma bruh!</h1>')
-})
-
-app.get('/instagram', (req,res)=>{
-    res.send('<h1>Follow on @abhay_pandey_1</h1>')
-})
-
-app.listen(process.env.PORT, () => {
-    console.log(`Serving on localhost http://localhost:${process.env.PORT}`)
-})
+connectDB()
