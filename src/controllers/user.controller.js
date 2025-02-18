b449c72113164b441dbf6b997b95895259bad15d
Abhay Pandey <abhaymp09@gmail.com>
2025-02-18 21:01:21 +0530

    log out and login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -132,12 +132,51 @@ const loginUser = asyncHandler(async (req, res) => {
         secure: true
     }
 
-    
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(
+        200,
+        {
+            user: loggedInUser, accessToken, refreshToken,
+        },
+        "User logged in successfully"
+    )
+)
 })
 
-const logoutUser = asyncHandler(async (req, res) => {})
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined,
+
+            },
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {},"User Logged Out." )
+    )
+
+})
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
